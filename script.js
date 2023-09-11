9e165834419ee768be40c162847d2470b9cdb388
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-09-12 00:00:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+// Get the textarea element and the h3 element
+const evaluatedText = document.getElementById("evaluatedText");
+const wordCount = document.getElementById("wordCount");
+
+// Function to count words
+function countWords() {
+    const text = evaluatedText.value.trim();
+    const words = text === "" ? 0 : text.split(/\s+/).length;
+    return words;
+}
+
+// Function to update the word count
+function updateWordCount() {
+    const words = countWords();
+    wordCount.textContent = words;
+}
+
+// Add an event listener to the textarea to update word count in real-time
+evaluatedText.addEventListener("input", updateWordCount);
+
+// Initial word count
+updateWordCount();
+
